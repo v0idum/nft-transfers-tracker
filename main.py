e588f64b4edae3a48fbcf9ba0cf56a1913ca5f9d
Akmal Melibaev <akmal.melibaev@gmail.com>
2021-10-22 00:09:29 +0500

    etherscan api usage for getting ERC721 transfers

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from aiogram.types import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 from aiogram.utils.markdown import text, hlink
 from aiohttp import ClientSession
-from loguru import logger
 from dotenv import load_dotenv
+from loguru import logger
 
 from sqliter import SQLighter
 from utils import timestamp
@@ -21,8 +21,7 @@ BITQUERY_API_KEY = os.getenv('BITQUERY_API_KEY')
 ETHERSCAN_API_KEY = os.getenv('ETHERSCAN_API_KEY')
 
 BITQUERY_URL = 'https://graphql.bitquery.io/'
-GET_TRANSFERS_QUERY = 'query ($network: EthereumNetwork!, $hash: String!, $limit: Int!, $offset: Int!) {\n  ethereum(network: $network) {\n    transfers(\n      options: {limit: $limit, offset: $offset}\n      amount: {gt: 0}\n      txHash: {is: $hash}\n    ) {\n      sender {\n        address\n        annotation\n      }\n      receiver {\n        address\n        annotation\n      }\n      amount\n      currency {\n        symbol\n        address\n        tokenType\n        name\n      }\n    }\n  }\n}\n'
-GET_TXS_QUERY = 'query ($address: String!, $block: Int!) {\n  ethereum(network: ethereum) {\n    transactions(\n      height: {gt: $block}\n      options: {limit: 10, offset: 0}\n      any: [{txSender: {is: $address}}, {txTo: {is: $address}}]\n    ) {\n      block {\n        height\n        timestamp {\n          unixtime\n        }\n      }\n      hash\n      sender {\n        address\n      }\n      to {\n        address\n      }\n      success\n      value: amount\n      amount(in: USD)\n      eth_fee: gasValue\n      usd_fee: gasValue(in: USD)\n    }\n  }\n}\n'
+TX_FEES = 'query ($hash: String!) {\n  ethereum(network: ethereum) {\n    transactions(txHash: {is: $hash}, options: {limit: 1}) {\n      value: amount\n      usd_value: amount(in: USD)\n      fee: gasValue\n      usd_fee: gasValue(in: USD)\n    }\n  }\n}\n'
 
 EXPLORER_TX_URL = 'https://etherscan.com/tx/%s'
 EXPLORER_ADDRESS_URL = 'https://etherscan.com/address/%s'
@@ -30,6 +29,10 @@ EXPLORER_NFT_URL = 'https://etherscan.com/token/%s'
 
 LAST_BLOCK_URL = f'https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey={ETHERSCAN_API_KEY}'
 ADDRESS_CHECK_URL = f'https://api.etherscan.io/api?module=account&action=balance&address=%s&tag=latest&apikey={ETHERSCAN_API_KEY}'
+NFT_TXS_URL = 'https://api.etherscan.io/api?module=account&action=tokennfttx&address=%s&startblock=%d&endblock=999999999&sort=asc&apikey=%s'
+ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+OPENSEA_NFT_URL = 'https://opensea.io/assets/{}/{}'
 
 logger.add('app.log', format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}")
 
@@ -137,74 +140,48 @@ async def get_last_block(session: ClientSession):
 
 
 @logger.catch
-async def get_transactions(address: str, last_block: int, session: ClientSession):
-    variables = '{\n  \"address\": \"%s\",\n  \"block\": %d\n}'
-    async with session.post(BITQUERY_URL, headers={"X-API-KEY": BITQUERY_API_KEY},
-                            json={'query': GET_TXS_QUERY, 'variables': variables % (address, last_block)}) as res:
+async def get_nft_transfers(address: str, start_block: int, session: ClientSession):
+    async with session.get(NFT_TXS_URL % (address, start_block, ETHERSCAN_API_KEY)) as res:
         return await res.json()
 
 
 @logger.catch
-async def get_tx_transfers(tx_hash: str, session: ClientSession):
-    variables = '{\n  \"limit\": 10,\n  \"offset\": 0,\n  \"network\": \"ethereum\",\n  \"hash\": \"%s\"\n}'
+async def _get_tx_fees(tx_hash: str, session: ClientSession):
+    variables = '{\"hash\": \"%s\"}'
     async with session.post(BITQUERY_URL, headers={"X-API-KEY": BITQUERY_API_KEY},
-                            json={'query': GET_TRANSFERS_QUERY, 'variables': variables % tx_hash}) as res:
-        return await res.json()
+                            json={'query': TX_FEES, 'variables': variables % tx_hash}) as res:
+        fees = await res.json()
+        return fees['data']['ethereum']['transactions'][0]
 
 
-async def _get_nft_transfers(tx: dict, session: ClientSession):
-    transfers = await get_tx_transfers(tx['hash'], session)
-    transfers = transfers['data']['ethereum']['transfers']
-    result = []
-    if not transfers:
-        return result
-    for transfer in transfers:
-        if (transfer['currency']['tokenType'] == 'ERC721' or transfer['currency']['tokenType'] == 'ERC20') and (
-                transfer['currency']['symbol'] != 'WETH' and transfer['currency']['symbol'] != 'ETH'):
-            result.append(transfer)
-    return result
-
-
-def _form_transfers(transfers: list):
-    result = 'NFT Transferred:'
-    for transfer in transfers:
-        nft_name = transfer["currency"]["name"] if transfer["currency"]["name"] != '-' or not transfer["currency"][
-            "name"] else transfer["currency"]["address"]
-        result += text('',
-                       f'From➡: {hlink(transfer["sender"]["address"], EXPLORER_ADDRESS_URL % transfer["sender"]["address"])}',
-                       f'To⬅: {hlink(transfer["receiver"]["address"], EXPLORER_ADDRESS_URL % transfer["receiver"]["address"])}',
-                       f'NFT: {hlink(nft_name, EXPLORER_NFT_URL % transfer["currency"]["address"])}',
-                       sep='\n') + '\n'
-    return result
-
-
-def _form_message(tx: dict, wallet, transfers):
-    status = 'Success' if tx['success'] else 'Failed'
+async def _form_message(transfer: dict, wallet, session: ClientSession):
+    header = 'New Mint!' if transfer["from"] == ZERO_ADDRESS else 'New Transfer!'
+    nft = f'{transfer["tokenName"]} #{transfer["tokenID"]}'
+    fees = await _get_tx_fees(transfer['hash'], session)
     message = text(f'Wallet tracked: {hlink(wallet[3], EXPLORER_ADDRESS_URL % wallet[0])}',
-                   hlink('New Transfer!', EXPLORER_TX_URL % tx['hash']),
-                   f'From➡: {hlink(tx["sender"]["address"], EXPLORER_ADDRESS_URL % tx["sender"]["address"])}',
-                   f'To⬅: {hlink(tx["to"]["address"], EXPLORER_ADDRESS_URL % tx["to"]["address"])}',
-                   f'Status: {status}',
-                   f'Timestamp: {timestamp(tx["block"]["timestamp"]["unixtime"])}',
-                   _form_transfers(transfers),
-                   f'Transaction Value💰: {round(tx["value"], 5)} Ether (${round(tx["amount"], 2)})',
-                   f'Transaction Fee💲: {round(tx["eth_fee"], 5)} Ether (${round(tx["usd_fee"], 2)})',
+                   hlink(header, EXPLORER_TX_URL % transfer['hash']),
+                   f'From➡: {hlink(transfer["from"], EXPLORER_ADDRESS_URL % transfer["from"])}',
+                   f'To⬅: {hlink(transfer["to"], EXPLORER_ADDRESS_URL % transfer["to"])}',
+                   f'NFT: {hlink(nft, OPENSEA_NFT_URL.format(transfer["contractAddress"], transfer["tokenID"]))}',
+                   'Status: Success',
+                   f'Timestamp: {timestamp(transfer["timeStamp"])}',
+                   f'Transaction Value💰: {round(fees["value"], 5)} Ether (${round(fees["usd_value"], 2)})',
+                   f'Transaction Fee💲: {round(fees["fee"], 5)} Ether (${round(fees["usd_fee"], 2)})',
                    sep='\n\n')
     return message
 
 
 @logger.catch
-async def new_tx_alert(txs: list, wallet, session: ClientSession):
-    for tx in txs:
-        transfers = await _get_nft_transfers(tx, session)
-        if not transfers:
-            continue
-        notification = _form_message(tx, wallet, transfers)
-        logger.info('New Transfer Alert!')
+async def new_tx_alert(transfers: list, wallet, session: ClientSession):
+    for transfer in transfers:
+        notification = await _form_message(transfer, wallet, session)
         await bot.send_message(wallet[2], notification)
+        logger.info('New Transfer Alert!')
+        await asyncio.sleep(0.5)
 
-    db.update_block(txs[-1]['block']['height'], wallet[0], wallet[2])
-    logger.info(f"Block updated to {txs[-1]['block']['height']}")
+    new_block = int(transfers[-1]['blockNumber']) + 1
+    db.update_block(new_block, wallet[0], wallet[2])
+    logger.info(f"Block updated to {new_block}")
 
 
 @logger.catch
@@ -214,17 +191,17 @@ async def track_wallets():
         session = ClientSession()
         try:
             for wallet in db.get_all_wallets():
-                txs = await get_transactions(wallet[0], wallet[1], session)
-                if txs and txs['data']['ethereum']['transactions']:
-                    logger.info(f'New tx to {wallet[0]}')
-                    await new_tx_alert(txs['data']['ethereum']['transactions'], wallet, session)
+                transfers = await get_nft_transfers(wallet[0], wallet[1], session)
+                if transfers and transfers['result']:
+                    logger.info(f'New nft transfers to {wallet[3]}')
+                    await new_tx_alert(transfers['result'], wallet, session)
                 await asyncio.sleep(2)
             await session.close()
             await asyncio.sleep(5)
         except Exception as e:
-            logger.exception(f'Exception', exc_info=e)
+            logger.exception(f'Exception while tracking:', exc_info=e)
             await session.close()
-            await asyncio.sleep(5)
+            await asyncio.sleep(2)
 
 
 async def on_bot_start_up(dispatcher) -> None:
--- utils.py
+++ utils.py
@@ -2,4 +2,4 @@ import time
 
 
 def timestamp(block_time):
-    return time.ctime(block_time)
+    return time.ctime(float(block_time))
